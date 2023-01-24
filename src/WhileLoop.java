b414a0a725fdf02e96559aac08f268fd8bdf522d
Ifenna Nwafor <90332645+King-Jasper@users.noreply.github.com>
2023-01-24 19:57:00 +0100

    initial commit

---
+++ src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int i = 11;
+        while(i > 0) {
+            i--;
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/Worker.java
@@ -0,0 +1,23 @@
+//public class Worker {
+//    private String name;
+//    private String birthdDate;
+//    protected String endDate;
+//public Worker(){
+//
+//}
+//    public Worker(String name, String birthDate) {
+//        this.name = name;
+//        this.birthdDate = birthDate;
+//    }
+//    public int getAge(){
+//    int currentYear = 2023;
+//    int birthDate = Integer.parseInt(birthDate.substring(6));
+//    return(currentYear-birthDate);
+//    }
+//    public double collectPay(){
+//    return 0.0;
+//    }
+//    public void terminate(String endDate){
+//    this.endDate = endDate;
+//    }
+//}
